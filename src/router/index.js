28f040c18a6b1d9b7e7ceaf8477e0e7759c387ea
Panagiotis Pantazopoulos <takispadaz@gmail.com>
2020-10-04 17:12:00 +0300

    add authentication guard to router

--- src/router/index.js
+++ src/router/index.js
@@ -5,47 +5,61 @@ import Login from '../views/Login.vue'
 import Signup from '../views/Signup.vue'
 import Logs from '../views/Logs.vue'
 import Notification from '../views/Notification.vue'
-
+import store from '../store/index'
+ 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requireAuthentication: true 
+    }
   },
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: {
+      requireAuthentication: false 
+    }
   },
   {
     path: '/signup',
     name: 'Signup',
-    component: Signup
+    component: Signup,
+    meta: {
+      requireAuthentication: false 
+    }
   },
   {
     path: '/logs',
     name: 'Logs',
-    component: Logs
+    component: Logs,
+    meta: {
+      requireAuthentication: true 
+    }
   },
   {
     path: '/notification',
     name: 'Notification',
-    component: Notification
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Notification,
+    meta: {
+      requireAuthentication: true 
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach(function(to, from, next){
+  if (to.meta.requireAuthentication && !store.getters.isAuthenticated)
+    next('/login');
+  next();
+});
 
 export default router
